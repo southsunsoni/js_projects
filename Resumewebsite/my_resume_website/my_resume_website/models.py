1045f9f71a9264978b37b38a83072c8930b737af
southsunsoni <Ivan.soni-tsafack@tu-ilmenau.de>
2023-12-26 05:54:27 +0100

    new

--- Resumewebsite/my_resume_website/my_resume_website/models.py
+++ Resumewebsite/my_resume_website/my_resume_website/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-class Tasch(models.Model):
+class Tasch(models.model):
     task=models.TextField()
     slug=models.SlugField(max_length=100)
     date=models.DateTimeField(auto_now_add=True)
+
+
 
